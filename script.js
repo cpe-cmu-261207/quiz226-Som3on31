fc823b440ecaaff8fbf6a2a558b01a2fd0a955b8
Som3on31 <saharit.f4@gmail.com>
2021-08-05 14:06:04 +0700

    kek

--- script.js
+++ script.js
@@ -6,8 +6,24 @@ const color = document.querySelector('#color')
 
 // define more constants and variables here
 
+let clickcount = 0
+
 btn_toggle.onclick = () => {
-  // your code here
+  clickcount++
+  if (clickcount % 2 === 0) {
+    author.innerHTML = ''
+    calculation.innerHTML = 630610767 + length.value
+    btn_toggle.innerHTML = 'Show Author'
+  } else {
+    author.innerHTML = ''
+    calculation.innerHTML = 630610767 + length.value
+    btn_toggle.innerHTML = 'Show Calculation'
+  }
 }
 
 // more codes for Search and Reset buttons here
+
+const context = document.getElementById('text')
+
+
+
